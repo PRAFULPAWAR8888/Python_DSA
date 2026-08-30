7bb741179f67a1e5a23a804e4073343c26603564
Praful Pawar <pawarpraful950@gmail.com>
2026-08-30 13:56:34 +0530

    buble sort

--- bubleSort.py
+++ bubleSort.py
@@ -1,1 +1,21 @@
-print("hello world")
+
+def b_sort(nums):
+    
+    n = len(nums)
+    
+    for i in range( n):
+        for j in range(n -i -1): 
+          if nums[i] > nums[i + 1]:
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+    
+    return nums  
+
+print(b_sort([1,2,34,2,1]))
+
+
+
+
+    
+
+
+
