fcfee0362c4ddb18b17a6bfad74d23adb8fa2cc1
Praful Pawar <pawarpraful950@gmail.com>
2026-09-04 15:55:16 +0530

    new change

--- largest_element_In_array.py
+++ largest_element_In_array.py
@@ -1,2 +1,16 @@
-print("hello world")
+
+def largest_element(arr):
+
+    if not arr:
+        return "arr not inserted"
+    largest = None
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] < arr[j]:
+                largest = arr[j]
+
+    return largest
+
+print(largest_element([2,3,41,313,42]))
+
              
